163dbb076c39f941d2e4ffe0c532729aad595025
mockairplane700 <sizibamthandazo@yahoo.com>
2025-01-26 15:35:01 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import ColorGradientGenerator from './components/color_gradients/color-gradients';
+
+function App() {
+  return(
+    <ColorGradientGenerator />
+  );
+}
+
+export default App
